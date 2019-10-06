5af03fca779adecd0fdfbb65a15af93354fbc84e
yaseito <patrickborjali792@gmail.com>
2019-10-06 12:24:55 -0500

    Hibernate v1

---
+++ src/com/demo/controlador/controladorProveedor.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.controlador;
+
+import com.demo.modelo.Marca;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ *
+ * @author Hijos
+ */
+public class controladorProveedor {
+
+    private Session sesion;
+
+    private void iniciaOperacion() {
+        SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        sesion = sessionFactory.openSession();
+        sesion.getTransaction().begin();
+    }
+
+    private void terminaOperacion() {
+        sesion.getTransaction().commit();
+        sesion.close();
+    }
+
+    public int guardaMarca(Marca marca) {
+        int id = 0;
+        iniciaOperacion();
+        id = (int) sesion.save(marca);
+        terminaOperacion();
+        return id;
+    }
+
+    public void actualizaMarca(Marca marca) {
+        iniciaOperacion();
+        sesion.update(marca);
+        terminaOperacion();
+    }
+
+    public void eliminaMarca(Marca marca) {
+        iniciaOperacion();
+        sesion.delete(marca);
+        terminaOperacion();
+    }
+
+    public Marca buscarMarca(int idMarca) {
+        Marca marca = null;
+
+        iniciaOperacion();
+        marca = (Marca) sesion.get(Marca.class, idMarca);
+        terminaOperacion();
+        return marca;
+    }
+
+    public List<Marca> listarMarca() {
+        List<Marca> listaPersona = null;
+        iniciaOperacion();
+        listaPersona = sesion.createQuery("from Marca").list();
+        terminaOperacion();
+        return listaPersona;
+    }    
+}
---
+++ src/com/demo/principal/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.principal;
+
+import com.demo.vista.vistaPrincipal;
+
+/**
+ *
+ * @author Hijos
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        vistaPrincipal vp = new vistaPrincipal();
+        vp.setVisible(true);
+    }
+   
+}
